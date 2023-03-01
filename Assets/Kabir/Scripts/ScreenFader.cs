cf88f16aa68cafd626d7aa30ef71093ec25376be
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2023-03-01 17:17:37 +0600

    IS-6
    includes fadein-fadeout

---
+++ Assets/Kabir/Scripts/ScreenFader.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public IEnumerator FadeIn()
+    {
+        Color color = fadeImage.color;
+
+        float timer = fadeDuration;
+        while (timer > 0f)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, timer / fadeDuration);
+            color.a = alpha;
+            fadeImage.color = color;
+
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(false);
+    }
+}
