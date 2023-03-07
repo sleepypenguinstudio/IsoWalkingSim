4b1116f258502004c1f2788b1401bab8d1df00bc
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2023-03-07 12:31:35 +0600

    racoo

--- Assets/Kabir/Scripts/BirdController.cs
+++ Assets/Kabir/Scripts/BirdController.cs
@@ -9,28 +9,60 @@ public class BirdController : MonoBehaviour
 
     private int currentPositionIndex = 0;
 
+    //private void Start()
+    //{
+    //    MoveToNextPosition();
+    //}
+
+    //private void MoveToNextPosition()
+    //{
+    //    currentPositionIndex++;
+
+    //    if (currentPositionIndex >= targetPositions.Length)
+    //    {
+    //        currentPositionIndex = 0;
+    //    }
+
+    //    Vector3 nextPosition = targetPositions[currentPositionIndex].position;
+
+    //    transform.DOMove(nextPosition, flySpeed).SetEase(Ease.Linear).OnComplete(() =>
+    //    {
+    //        // Rotate towards the next position when the bird reaches the current position
+    //        Vector3 nextDirection = targetPositions[(currentPositionIndex + 1) % targetPositions.Length].position - transform.position;
+    //        transform.DOLookAt(transform.position + nextDirection, rotationSpeed, AxisConstraint.Y);
+    //        MoveToNextPosition();
+    //    });
+    //}
+
     private void Start()
     {
-        MoveToNextPosition();
+        currentPositionIndex = 0;
+        transform.position = targetPositions[currentPositionIndex].position;
+
+        Vector3 nextPosition = targetPositions[(currentPositionIndex + 1) % targetPositions.Length].position;
+        Vector3 nextDirection = nextPosition - transform.position;
+        transform.DOLookAt(transform.position + nextDirection, rotationSpeed, AxisConstraint.Y).OnComplete(() =>
+        {
+            MoveToNextPosition();
+        });
     }
 
     private void MoveToNextPosition()
     {
         currentPositionIndex++;
-
         if (currentPositionIndex >= targetPositions.Length)
         {
             currentPositionIndex = 0;
         }
 
         Vector3 nextPosition = targetPositions[currentPositionIndex].position;
-
+        Vector3 nextDirection = nextPosition - transform.position;
+        transform.DOLookAt(transform.position + nextDirection, rotationSpeed, AxisConstraint.Y);
         transform.DOMove(nextPosition, flySpeed).SetEase(Ease.Linear).OnComplete(() =>
         {
-            // Rotate towards the next position when the bird reaches the current position
-            Vector3 nextDirection = targetPositions[(currentPositionIndex + 1) % targetPositions.Length].position - transform.position;
-            transform.DOLookAt(transform.position + nextDirection, rotationSpeed, AxisConstraint.Y);
             MoveToNextPosition();
         });
     }
+
+
 }
