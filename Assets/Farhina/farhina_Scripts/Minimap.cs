240960a04bb6d89f2fddf6c81ac10687caaaee48
Farhina Afrin <farhinafrin01@gmail.com>
2023-03-07 09:52:58 +0600

    minimap

--- Assets/Farhina/farhina_Scripts/Minimap.cs
+++ Assets/Farhina/farhina_Scripts/Minimap.cs
@@ -8,10 +8,10 @@ public class Minimap : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform player;
-    public CinemachineVirtualCamera cineMachineMiniMapCamera;
+    //public CinemachineVirtualCamera cineMachineMiniMapCamera;
     public Camera miniMapCamera;
     public RawImage miniMapImage;
-    public GameObject miniMappointerImage;
+    //public GameObject miniMappointerImage;
     public Vector3 offset;
     public static Minimap _instance;
 
@@ -37,11 +37,11 @@ public class Minimap : MonoBehaviour
     private void LateUpdate()
     {
         // Update the position of the mini map camera to follow the player
-        miniMappointerImage.transform.position = new Vector3(player.position.x, player.position.y + 1, player.position.z);
-        cineMachineMiniMapCamera.transform.position = new Vector3(player.position.x, cineMachineMiniMapCamera.transform.position.y, player.position.z);
+        //miniMappointerImage.transform.position = new Vector3(player.position.x, player.position.y + 1, player.position.z);
+        miniMapCamera.transform.position = new Vector3(player.position.x, miniMapCamera.transform.position.y, player.position.z);
 
         // Rotate the mini map camera to match the orientation of the player
-        cineMachineMiniMapCamera.transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        miniMapCamera.transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
 
 
     }
